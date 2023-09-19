3d679b3bb84c4daa323bcea4a7b6936273774398
vedanti <vedanti.r.bagade@gmail.com>
2023-09-19 20:56:12 +0530

    new file added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React Logo' className='nav--icon'/>
+      <h3 className='nav--logo_text'>ReactFacts</h3>
+      <h4 className='nav--title'>Home</h4>
+    </nav>
+  )
+}
+
+function MainContent() {
+  return (
+    <main>
+      <h1 className='main--title'>Fun facts about React</h1>
+      <ul className='main--facts'>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
+ }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <MainContent />
     </div>
   );
 }
